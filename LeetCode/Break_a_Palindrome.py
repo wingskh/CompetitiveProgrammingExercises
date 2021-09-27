41bf3f23aa366c69d92e4069d7f7e5d20fcbb59e
wingskh <deathlwing@gmail.com>
2021-09-28 01:10:41 +0800

    Added a LeetCode Question: Longest_Palindromic_Substring

--- LeetCode/Break_a_Palindrome.py
+++ LeetCode/Break_a_Palindrome.py
@@ -1,25 +1,23 @@
-def break_a_palindrome(palindrome):
-    if len(palindrome) < 2:
-        return ""
-    
-    for i in range(len(palindrome)):
-        if i == (len(palindrome)-1)/2:
-            continue
-        if i != len(palindrome) - 1:
-            # reverse_index = len(palindrome) - i -1
-            if ord(palindrome[i]) > 97:
-                palindrome = palindrome[:i] + 'a' + palindrome[i+1:]
-                return palindrome
-            else:
+# https://leetcode.com/problems/break-a-palindrome/
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) < 2:
+            return ""
+        
+        for i in range(len(palindrome)):
+            if i == (len(palindrome)-1)/2:
                 continue
-        else:
-            if ord(palindrome[i]) > 97:
-                palindrome = palindrome[:-1]+'a'
+            if i != len(palindrome) - 1:
+                # reverse_index = len(palindrome) - i -1
+                if ord(palindrome[i]) > 97:
+                    palindrome = palindrome[:i] + 'a' + palindrome[i+1:]
+                    return palindrome
+                else:
+                    continue
             else:
-                palindrome = palindrome[:-1]+chr(ord(palindrome[i])+1)
-                
-    return palindrome
-
-
-palindrome = "aba"
-print(break_a_palindrome(palindrome))
+                if ord(palindrome[i]) > 97:
+                    palindrome = palindrome[:-1]+'a'
+                else:
+                    palindrome = palindrome[:-1]+chr(ord(palindrome[i])+1)
+                    
+        return palindrome
---
+++ LeetCode/Longest_Palindromic_Substring.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest_palindromic = ""
+        max_len = 0
+        max_right_index =  len(s)
+        for index in range(len(s)):
+            # Detect palindromic with odd length
+            left_index, right_index = index, index
+            while left_index >= 0 and right_index < max_right_index and s[left_index] == s[right_index]:
+                if right_index - left_index + 1 >= max_len:
+                    longest_palindromic = s[left_index:right_index + 1]
+                    max_len = right_index - left_index + 1
+                left_index -= 1
+                right_index += 1
+
+            # Detect palindromic with even length
+            left_index, right_index = index, index + 1
+            while left_index >= 0 and right_index < max_right_index and s[left_index] == s[right_index]:
+                if right_index - left_index + 1 >= max_len:
+                    longest_palindromic = s[left_index:right_index + 1]
+                    max_len =  right_index - left_index + 1
+                left_index -= 1
+                right_index += 1
+
+        return longest_palindromic
