507c0ccfd3276ae2a0804a3cdb0e5118e0c45447
wingskh <40230493+wingskh@users.noreply.github.com>
2021-10-04 18:47:09 +0800

    Added LeetCode problem: Delete Operation for Two Strings

---
+++ LeetCode/Delete_Operation_for_Two_Strings.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/delete-operation-for-two-strings/
+class Solution:
+    word1_length = len(word1)
+    word2_length = len(word2)
+
+    min_distance_dp = [[i for i in range(word2_length+1)]] + [[i] + [math.inf] * (word2_length) for i in range(1, word1_length+1)]
+
+    for i in range(word1_length):
+        for j in range(word2_length):
+            min_distance_dp[i + 1][j + 1] = min_distance_dp[i][j] if word1[i] == word2[j] else min(min_distance_dp[i+1][j], min_distance_dp[i][j+1]) + 1
+
+    return min_distance_dp[-1][-1]
