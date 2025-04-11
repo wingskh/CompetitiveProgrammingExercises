ac6cd02cd896588013b300e042f3a997f62adb53
wingsun <wingsun@vgt-hk.net>
2025-04-11 13:53:08 +0800

    feat: updated code

--- LeetCode/Min_Cost_to_Connect_All_Points.py
+++ LeetCode/Min_Cost_to_Connect_All_Points.py
@@ -1,25 +1,29 @@
 # https://leetcode.com/problems/min-cost-to-connect-all-points/
-
 from typing import List
 
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         def findRoot(i):
-            if parent_list[i] == i:
-                return i
-            return findRoot(parent_list[i])
+            if i != parent_list[i]:
+                parent_list[i] = findRoot(parent_list[i])
+            return parent_list[i]
 
         def manhattan_distance(p1, p2):
             return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
-        def union(i_root, j_root):
-            if rank_list[i_root] < rank_list[j_root]:
-                parent_list[i_root] = j_root
-            elif rank_list[i_root] >= rank_list[j_root]:
-                if rank_list[i_root] == rank_list[j_root]:
-                    rank_list[i_root] += 1
-                parent_list[j_root] = i_root
+        def union(i, j):
+            i_root = findRoot(i)
+            j_root = findRoot(j)
+            if i_root != j_root:
+                if rank_list[i_root] < rank_list[j_root]:
+                    parent_list[i_root] = j_root
+                elif rank_list[i_root] >= rank_list[j_root]:
+                    if rank_list[i_root] == rank_list[j_root]:
+                        rank_list[i_root] += 1
+                    parent_list[j_root] = i_root
+                return True
+            return False
 
         point_len = len(points)
         edges = [
@@ -33,22 +37,18 @@ class Solution:
         result = 0
         while len(edges) > 0:
             edge, i, j = edges.pop()
-            i_root = findRoot(i)
-            j_root = findRoot(j)
-            if i_root != j_root:
-                union(i_root, j_root)
+            is_union = union(i, j)
+            if is_union:
                 result += edge
+
         return result
 
 
 points = [[0, 0], [2, 2], [3, 10], [5, 2], [7, 0]]
 # Output: 20
-
 points = [[3, 12], [-2, 5], [-4, 1]]
 # # Output: 18
-
 points = [[-1000000, -1000000], [1000000, 1000000]]
 # Output: 4000000
-
 sol = Solution()
 print(sol.minCostConnectPoints(points))
